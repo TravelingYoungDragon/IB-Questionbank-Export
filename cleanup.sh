fea44df124eff36e81c23b456949f1993efdd6e9
IBanon <anonssecureemail@protonmail.com>
2019-11-04 18:25:29 -0500

    added branch for pages

--- cleanup.sh
+++
@@ -1,4 +0,0 @@
-find . -type f -name 'index*.html' -delete
-find . -type f -name 'ui-*' -delete
-find . -type f -name 'chosen-sprite*' -delete
-find . -type f -name '863c5a2796b088dea28a1f75a0973372*' -delete
